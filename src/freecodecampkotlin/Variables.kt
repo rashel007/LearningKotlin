c2f30f5d8a8030736c60fe39f424c20692495b76
EA Rashel <rashel320@gmail.com>
2019-10-07 00:06:51 +0600

    first commit

---
+++ src/freecodecampkotlin/Variables.kt
@@ -0,0 +1,22 @@
+package freecodecampkotlin
+
+
+/**
+ * We can declare variables in kotlin by using var or val keyword
+ *  -> we have to use var or val to declare variable
+ *  -> var is mutable
+ *  -> val is immutable
+ *
+ * Declaring type is optional in kotlin.
+ *  -> to declare a type we have to use colon(:) after the variable and then the type
+ *
+ * to declare a variable null we should use variable type with a ? at end and then
+ * set the value to null
+ */
+fun main() {
+
+    var name: String? = null
+    var name2: String = "Rashel 2"
+    var name3 = "Rashel 3"
+    val name4 = "Rashel 4"
+}
