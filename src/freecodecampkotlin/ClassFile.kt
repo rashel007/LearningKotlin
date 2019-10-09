fa8178e7c36ed6c13fc52f7ab2f3ed4c28bc779e
EA Rashel <rashel320@gmail.com>
2019-10-09 23:24:25 +0600

    learned creating class

---
+++ src/freecodecampkotlin/ClassFile.kt
@@ -0,0 +1,14 @@
+package freecodecampkotlin
+
+fun main() {
+
+
+    val myClass = FirstClass()
+//    myClass.firstName
+////    myClass.lastName
+////    myClass.nickName = "GabberSing"
+////    myClass.nickName = "new NickName"
+////    println(myClass.nickName)
+
+    myClass.printInfo()
+}
